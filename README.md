43828876f9b35ff49995a72e9d17053c012181a0
cng6sk <838928964@qq.com>
2023-08-06 22:57:06 +0800

    add auto-indent brackets

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This plugin for Obsidian improves the code blocks to make it more like a regular
 # Action in Code Block
 
 - uses the tab key to insert tabs instead of indenting
-
+- uses the enter key to auto-indent brackets 
--- main.ts
+++ main.ts
@@ -5,8 +5,12 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 var editor;
 
 var select2LineHome = "";
+var bracketsIndex = "";
+var preCursorLine = "";
 var entireDoc;
 var currentCursorLoc;
+var oneWordBeforeCursorLoc;
+var oneWordAfterCursorLoc ;
 var currentLineNoLoc;
 
 export default class MyPlugin extends Plugin {
@@ -29,8 +33,10 @@ export default class MyPlugin extends Plugin {
 		// console.log(entireDoc);
 		currentCursorLoc = editor.getCursor();
 		currentLineNoLoc = currentCursorLoc.line;
-		select2LineHome = editor.getRange({line:currentLineNoLoc, ch:0}, currentCursorLoc);
-		
+		select2LineHome = editor.getRange({line:currentLineNoLoc, ch: 0}, currentCursorLoc);
+		preCursorLine = editor.getLine(currentLineNoLoc - 1);
+		oneWordBeforeCursorLoc = editor.getRange({line: currentLineNoLoc, ch: select2LineHome.length - 1}, currentCursorLoc);
+		oneWordAfterCursorLoc  = editor.getRange(currentCursorLoc, {line: currentLineNoLoc, ch: select2LineHome.length + 1});
 	}
 
 	inCodeBlock(): boolean {
@@ -49,6 +55,39 @@ export default class MyPlugin extends Plugin {
 		return false;
 	}
 
+	inBrackets(): number {
+		console.log("inBracketsjudge");
+		console.log(preCursorLine);
+		console.log(oneWordAfterCursorLoc);
+		var preflag: number = 0;
+		var bckflag: number = 0;
+		if (preCursorLine.match(/\($/)) {
+			preflag = 1;
+		}
+		else if (preCursorLine.match(/\[$/)) {
+			preflag = 2;
+		}
+		else if (preCursorLine.match(/\{$/)) {
+			preflag = 3;
+		}
+
+		if (oneWordAfterCursorLoc .match(/\)/)) {
+			bckflag = 1;
+		}
+		else if (oneWordAfterCursorLoc .match(/\]/)) {
+			bckflag = 2;
+		}
+		else if (oneWordAfterCursorLoc .match(/\}/)) {
+			bckflag = 3;
+		}
+		if (preflag && preflag == bckflag) {
+			console.log("inBrackets");
+			return preflag;
+		}
+
+		return 0;
+	}
+
 	getFullText() {
 		var editorTmp = this.getEditor();
 		if (!editorTmp) {
@@ -61,6 +100,7 @@ export default class MyPlugin extends Plugin {
 			return;
 		}
 	}
+
 	
 
 	async onload() {
@@ -80,10 +120,44 @@ export default class MyPlugin extends Plugin {
 					select2LineHome = select2LineHome.replace(/(    |\t)/,"");
 					entireDoc.replaceRange(select2LineHome+"	", {line: currentLineNoLoc, ch: 0}, currentCursorLoc);
 				}
-				
+			}
+			else if (e.key == "Enter") {
+				var typeCheck = this.inBrackets();
+				var pre = "";
+				var bck = "";
+				if (this.inCodeBlock() && typeCheck) {
+					switch (typeCheck) {
+						case 1:
+							pre = "(";
+							bck = ")";
+							break;
+						case 2:
+							pre = "[";
+							bck = "]";
+							break;
+						case 3:
+							pre = "{";
+							bck = "}";
+							break;
+						default:
+							break;
+					}
+					// console.log(select2LineHome);
+					select2LineHome = select2LineHome.replace(/[^    |\t]/, "");
+					// console.log(select2LineHome);
+					bracketsIndex = pre + '\n' + select2LineHome + "    \n" + select2LineHome + bck;
+					// console.log(bracketsIndex);
+					entireDoc.replaceRange(bracketsIndex, 
+										  	{line: currentLineNoLoc - 1, ch: preCursorLine.length - 1}, 
+											{
+												line: currentLineNoLoc, ch: select2LineHome.length + 1
+											});
+					entireDoc.setCursor(currentLineNoLoc, select2LineHome.length + 4);
+				}
 			}
         });
 	}
+
 
 	onunload() {
 		console.log('unloading obsidian-refinedCodeBlock');
